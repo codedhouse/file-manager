a7ce9647a9e3c6a88c97aa4b661b0600ac5cd2e4
Vivien Keegan <vivienkeegan@gmail.com>
2016-07-19 22:12:29 -0400

    List only files that can be read by app

--- FileManager/app/src/main/java/vivienkeegan/csc4320/filemanager/ListAdapter.java
+++ FileManager/app/src/main/java/vivienkeegan/csc4320/filemanager/ListAdapter.java
@@ -1,7 +1,7 @@
 package vivienkeegan.csc4320.filemanager;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,36 +14,30 @@ import java.io.File;
 import java.util.List;
 
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ItemViewHolder> {
-
-    public static class ItemViewHolder extends RecyclerView.ViewHolder
-            implements View.OnClickListener {
-        public ImageView mImageView;
-        public TextView mTextView;
-        public RelativeLayout mRelativeLayout;
-
-        public ItemViewHolder(View itemView) {
-            super(itemView);
-            itemView.setOnClickListener(this);
-            mImageView = (ImageView) itemView.findViewById(R.id.item_icon);
-            mTextView = (TextView) itemView.findViewById(R.id.item_name);
-            mRelativeLayout = (RelativeLayout) itemView.findViewById(R.id.item_layout);
-        }
-
-        @Override
-        public void onClick(View itemView) {
-            Log.i("HELLO", "Position " + getLayoutPosition());
-            Toast.makeText(itemView.getContext(),
-                    "position " + getLayoutPosition(),
-                    Toast.LENGTH_LONG).show();
-        }
-
-    }
-
     private List<File> mFileList;
-
 
     public ListAdapter(List<File> fileList) {
         mFileList = fileList;
+    }
+
+    public boolean createDirectory(File newDir) {
+        return (newDir.mkdir());
+    }
+
+    public boolean deleteFile(int position, Context context) {
+        try {
+            File f = mFileList.get(position);
+            if (f.delete()) {
+                mFileList.remove(position);
+            } else {
+                return false;
+            }
+        } catch (SecurityException se) {
+            Toast.makeText(context, "Permission denied", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
     }
 
     @Override
@@ -69,5 +63,32 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ItemViewHolder
                 inflate(R.layout.list_item_layout, vg, false);
 
         return new ItemViewHolder(itemView);
+    }
+
+    public static class ItemViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+        public ImageView mImageView;
+        public TextView mTextView;
+        public RelativeLayout mRelativeLayout;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            mImageView = (ImageView) itemView.findViewById(R.id.item_icon);
+            mTextView = (TextView) itemView.findViewById(R.id.item_name);
+            mRelativeLayout = (RelativeLayout) itemView.findViewById(R.id.item_layout);
+        }
+
+        @Override
+        public void onClick(View itemView) {
+            int position = getAdapterPosition();
+
+            MainActivity context = (MainActivity) itemView.getContext();
+            if (context.deleteFile(position)) {
+                Toast.makeText(context, "File deleted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "Unable to delete", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- FileManager/app/src/main/java/vivienkeegan/csc4320/filemanager/MainActivity.java
+++ FileManager/app/src/main/java/vivienkeegan/csc4320/filemanager/MainActivity.java
@@ -1,13 +1,16 @@
 package vivienkeegan.csc4320.filemanager;
 
-import android.content.Context;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,14 +22,16 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private String currentDirectory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        File root = new File(ROOT);
-        mFileList = Arrays.asList(root.listFiles());
+        currentDirectory = ROOT;
+        //Log.i("fff", Environment.getDataDirectory().getAbsolutePath());
+        mFileList = createFileList(currentDirectory);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.file_recycler_view);
 
@@ -38,5 +43,46 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(mLayoutManager);
         mAdapter = new ListAdapter(mFileList);
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+    protected List<File> createFileList(String path) {
+        List<File> fileList = new ArrayList<>();
+        File directory = new File(path);
+
+        if (directory.exists()) {
+            File[] files = directory.listFiles();
+            for (File f : files) {
+                if (f.canRead()) {
+                    fileList.add(f);
+                }
+            }
+        } else {
+            Log.i("FileManager: ", "Directory does not exist");
+        }
+
+        return fileList;
+    }
+
+    protected boolean deleteFile(int position) {
+        if (((ListAdapter) mAdapter).deleteFile(position, this)) {
+            mAdapter.notifyDataSetChanged();
+           return true;
+        }
+        return false;
+    }
+
+    protected boolean createFolder(View v) {
+        ListAdapter adapter = (ListAdapter) mAdapter;
+
+        File newDir = new File("/temp/");
+
+        if (!adapter.createDirectory(newDir)) {
+            Toast.makeText(this, "Unable to create", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        mFileList.add(newDir);
+        mAdapter.notifyDataSetChanged();
+        return true;
     }
 }
